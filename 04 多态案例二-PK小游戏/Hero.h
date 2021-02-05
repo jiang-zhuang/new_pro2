4b9638d56680487baf315dd7c31a2441ff7605cb
jiang <1324615201@qq.com>
2021-02-05 21:20:15 +0800

    新增项目文件

---
+++ 04 多态案例二-PK小游戏/Hero.h
@@ -0,0 +1,29 @@
+#pragma once 
+#include <iostream>
+#include "Weapon.h"
+#include <string>
+#include "Monster.h"
+using namespace std;
+
+class Monster;
+class Monster;
+
+class Hero
+{
+public:
+	Hero();
+
+	string m_Name; //人名
+
+	int m_Atk; //攻击力
+
+	int m_Def; // 防御力
+
+	int m_Hp; //血量
+
+	Weapon * weapon; //武器
+
+	void EquipWeapon(Weapon * weapon);
+
+	void Attack( Monster * monster );
+};
